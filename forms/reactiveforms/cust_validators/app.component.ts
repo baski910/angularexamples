3288283d210f197c7b04afd6795fe2c829f1b1a4
baski910 <42275121+baski910@users.noreply.github.com>
2025-08-28 13:43:33 +0530

    Update app.component.ts

--- forms/reactiveforms/cust_validators/app.component.ts
+++ forms/reactiveforms/cust_validators/app.component.ts
@@ -1,3 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule, AbstractControl, ValidationErrors } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
 export class AppComponent {
   title = 'cust-pass-validator';
   singleForm = new FormGroup({
